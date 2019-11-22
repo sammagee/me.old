51b45032800c50dee6251fa2c5ab09d51fd4d565
Sam <s@sammagee.me>
2019-11-22 11:37:45 -0600

    Add github link

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,8 +29,13 @@ function Index() {
           <a className="mx-4" href="#">Projects</a>
         </nav> */}
 
-        <div className="flex flex-1 justify-end">
-          <a className="contact-button bg-yellow-400 flex items-center px-6 py-3 rise rounded-full text-yellow-900 uppercase" href="mailto:s@sammagee.me">
+        <div className="flex flex-1 items-center justify-end">
+          <a className="bg-gray-800 p-4 rounded-full rise" href="https://github.com/sammagee">
+            <svg class="text-gray-500 h-6 w-6" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"/>
+            </svg>
+          </a>
+          <a className="contact-button bg-yellow-400 flex items-center ml-4 px-6 py-3 rise rounded-full text-yellow-900 uppercase" href="mailto:s@sammagee.me">
             <span className="mr-2 mt-1">Contact</span>
 
             <svg className="fill-current text-yellow-700 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
