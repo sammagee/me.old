7fca4c5eda25069bd49ca1a8ef9b847f6fca5e75
Sam <s@sammagee.me>
2019-11-25 11:03:25 -0600

    Alter github link styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,11 +30,22 @@ function Index() {
         </nav> */}
 
         <div className="flex flex-1 items-center justify-end">
-          <a className="bg-gray-800 p-4 rounded-full rise" href="https://github.com/sammagee">
-            <svg class="text-gray-500 h-6 w-6" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-              <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"/>
-            </svg>
-          </a>
+          <div className="navigation">
+            <a className="flex items-center" href="https://github.com/sammagee" target="_blank" rel="noopener noreferrer">
+              <svg className="text-gray-600 h-4 w-4" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0020 4.77 5.07 5.07 0 0019.91 1S18.73.65 16 2.48a13.38 13.38 0 00-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 005 4.77a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 009 18.13V22"/>
+              </svg>
+
+              <span className="link-text ml-1 mt-1 text-white">GitHub</span>
+
+              <svg className="ml-1 h-3 w-3 text-gray-700" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                <polyline points="15 3 21 3 21 9"></polyline>
+                <line x1="10" y1="14" x2="21" y2="3"></line>
+              </svg>
+            </a>
+          </div>
+
           <a className="contact-button bg-yellow-400 flex items-center ml-4 px-6 py-3 rise rounded-full text-yellow-900 uppercase" href="mailto:s@sammagee.me">
             <span className="mr-2 mt-1">Contact</span>
 
